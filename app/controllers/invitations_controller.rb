f42df0eb6894e82391d995dd895cd2616cfcf434
Rumsauce <tylerdownie0@gmail.com>
2021-02-26 14:43:50 -0800

    Add invitation button to event show page, and start work on accept button for event on welcome page

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,25 +1,46 @@
 class InvitationsController < ApplicationController
 
     def create
-      @invitation = Inviation.new(initation_params)
+      event = Event.find(params[:event_id])
+      @invitation = Invitation.new(invitation_params)
 
-      if invitation.save
-        invitation.invited!
+      if @invitation.save
+        @invitation.invited!
         flash[:notice] = "Invitation sent to user!"
-        redirect_to @event
+        redirect_to event_path(event)
       else
         flash[:alert] = "Invite could not be sent."
-        redirect_to @event
+        redirect_to event_path(event)
       end
     end
 
+    def update
+      event = Event.find(params[:event_id])
+      @invitation = Invitation.find(params[:id])
+
+      if @invitation && @invitation.invited?
+        @invitation.accepted!
+        flash[:notice] = "Invitation accepted."
+      else
+        flash[:notice] = "Name not on list."
+      end
+
+      redirect_to root_path
+    end
+
     def destroy
+      event = Event.find(params[:event_id])
+      @invitation = Invitation.find(params[:id])
+
+      @invitation.destroy
+
+      redirect_to event_path(event)
     end
 
     private 
 
       def invitation_params
-          params.require(:inviation).permit(:user_id, :event_id)
+          params.permit(:user_id, :event_id)
       end
 
 end
