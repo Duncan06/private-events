6f7aa2af5ed98e8c84e793b0b125de97ca7c2a97
Rumsauce <tylerdownie0@gmail.com>
2021-02-25 23:50:32 -0800

    Update events to contain invites

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
     belongs_to :creator, class_name: "User"
+    has_many :invitations
     has_many :event_attendances, foreign_key: :attended_event_id
     has_many :attendees, through: :event_attendances
     scope :past, -> { where('eventdate < ?', Date.today) }
