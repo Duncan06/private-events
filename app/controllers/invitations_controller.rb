a509621247f279a24c98f3767a9d6996ff7247ed
Rumsauce <tylerdownie0@gmail.com>
2021-02-26 22:54:26 -0800

    Refactor to incorporate invitation instead of event attendance

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,21 +1,20 @@
 class InvitationsController < ApplicationController
 
     def create
-      event = Event.find(params[:event_id])
+      @event = Event.find(params[:event_id])
       @invitation = Invitation.new(invitation_params)
 
       if @invitation.save
         @invitation.invited!
         flash[:notice] = "Invitation sent to user!"
-        redirect_to event_path(event)
+        redirect_to event_path(@event)
       else
         flash[:alert] = "Invite could not be sent."
-        redirect_to event_path(event)
+        redirect_to event_path(@event)
       end
     end
 
     def update
-      event = Event.find(params[:event_id])
       @invitation = Invitation.find(params[:id])
 
       if @invitation && @invitation.invited?
@@ -29,12 +28,12 @@ class InvitationsController < ApplicationController
     end
 
     def destroy
-      event = Event.find(params[:event_id])
+      @event = Event.find(params[:event_id])
       @invitation = Invitation.find(params[:id])
 
       @invitation.destroy
 
-      redirect_to event_path(event)
+      redirect_to event_path(@event)
     end
 
     private 
